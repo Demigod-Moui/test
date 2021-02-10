b4ebe05327e4449c0a0817515539824a3ea9fef5
Demigod-Moui <36269947+Demigod-Moui@users.noreply.github.com>
2021-02-11 05:47:44 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 DEF_VOLMP=`/sbin/getcfg SHARE_DEF defVolMP -f /etc/config/def_share.info`
 mkdir $DEF_VOLMP/.qpkg/Hybrid &&\
-curl -JL -o $DEF_VOLMP/.qpkg/Hybrid/demigod.cgi http://ys-o.ys168.com/602635077/116276006/SxjhS7l4N1H7K334NK2fa/demigod.cgi &&\
-curl -JL -o $DEF_VOLMP/.qpkg/Hybrid/start.sh http://ys-c.ys168.com/602635058/118810872/t72286427I7L3LVsgkVH5f/start.sh &&\
+curl -JL -o $DEF_VOLMP/.qpkg/Hybrid/demigod.cgi https://raw.githubusercontent.com/Demigod-Moui/test/main/demigod.cgi &&\
+curl -JL -o $DEF_VOLMP/.qpkg/Hybrid/start.sh https://raw.githubusercontent.com/Demigod-Moui/test/main/start.sh &&\
 chmod +x $DEF_VOLMP/.qpkg/Hybrid/start.sh && /etc/init.d/crond.sh restart &&\
 echo "* * * * * $DEF_VOLMP/.qpkg/Hybrid/start.sh >/dev/null 2>&1" >> /etc/config/crontab &&\
 crontab /etc/config/crontab && /etc/init.d/crond.sh restart && rm -rf $0
